24bf49e180ea096a608aa10ab3a356aa9ced6b9b
Tom Dorozenko <tom.dorozenko@curtin.edu.au>
2022-02-23 13:10:30 +0800

    setup querys

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,7 +1,9 @@
 import {hello} from "./query";
+import { getAllListings } from "./query"
 
 export const resolvers = {
     Query: {
-        hello: (root, args, context) => hello(args, context)
+        hello: (root, args, context) => hello(args, context),
+        getAllListings: (root, args, context) => getAllListings(args, context),
     }
 }
--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -1,3 +1,52 @@
+import * as dynamoDBLib from "../../libs/dynamodb-lib"
+
+const params = {
+    TableName: process.env.ListingsDB || "dev-listings",
+}
+
+try {
+    const result = await dynamoDBLib.call("scan", params)
+
+    if (result.Items.length === 0) {
+        return "You have no listings"
+    } else {
+        return result.Items.map(i => ({
+            listingId: i.listingId,
+            coverPhoto: i.coverPhoto,
+            listingName: i.listingName,
+            listingDescription: i.listingDescription,
+            listingType: i.listingType.map(m => ({
+                name: m,
+            })),
+            listingLocation: i.listingLocation,
+            listingActivities: i.listingActivities.map(k => ({
+                name: k,
+            })),
+            specialType: i.specialType,
+            specialAmount: i.specialAmount,
+            rating: i.rating,
+            guide: {
+                Name: i.guide.name,
+                Bio: i.guide.bio,
+                Avatar: i.guide.avatar,
+            },
+            price: i.price,
+            numberOfDays: i.numberOfDays,
+        }))
+    }
+
+    // return result;
+} catch (e) {
+    return {
+        message: e.message,
+        code: "500x",
+    }
+}
+
 export const hello = (args, context) => {
     return "Your GraphQL API is now LIVE!🎈 "
+}
+
+export const getAllListings = async (args, context) => {
+    return null
 }
